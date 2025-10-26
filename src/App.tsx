789ec4ed25893a43ab21fc9cf19cc7e9eaa7b424
Shangqi Xue <shangqixue@gmail.com>
2025-10-26 01:27:41 -0700

    favorite resturant

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {"101 Taiwanese Cuisine"} <span role="img" aria-label="thumbs up">👍</span> {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
